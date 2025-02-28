c9a7c8400d374aebe5860a2a67ba93c2fac8f402
MohamedBOULAALAM <mohamedboulaalam01@gmail.com>
2025-02-28 17:08:44 +0000

    Spring - Version xml :  1. Add a config.xml file.

--- src/main/java/metier/MetierImpl.java
+++ src/main/java/metier/MetierImpl.java
@@ -1,9 +1,15 @@
 package metier;
 import dao.DaoImpl;
+import dao.DaoImpl2;
 import dao.IDao;
 
 public class MetierImpl implements IMetier {
     private IDao dao ; // coupalge faible (depend d'interface)
+
+
+    public MetierImpl(IDao dao) { this.dao = dao;}
+    public MetierImpl() {}
+
     @Override
     public double calcul() {
         double t = dao.getData();
